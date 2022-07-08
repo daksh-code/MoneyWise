d162caa3699c2657cfda5447aaa96a55b674721b
Daksh Dagariya <dakshdagariya@Dakshs-MacBook-Air.local>
2022-07-09 02:31:08 +0530

    added eb package configurationssss

--- stockproject/settings.py
+++ stockproject/settings.py
@@ -88,8 +88,14 @@ ASGI_APPLICATION = "stockproject.asgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'User_Data',
+        'USER': 'postgres',
+        'PASSWORD': '     ',
+        'HOST': 'localhost',
+        'PORT':'5432',
+
+
     }
 }
 

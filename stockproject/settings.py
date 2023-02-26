28ca881bfe933de37f4d8a44956f720e3c6900e8
daksh <dagariyadaksh@gmail.com>
2023-02-27 02:12:00 +0530

    new code

--- stockproject/settings.py
+++ stockproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib import messages
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,20 +95,28 @@ DATABASES = {
 }
 
 """
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'User_Data',
-            'USER': 'postgres',
-            'PASSWORD': '     ',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'User_Data',
+        'USER': 'postgres',
+        'PASSWORD': '     ',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+}
 """
 
 
+"""
+DATABASES = {
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/stockproject',
+        conn_max_age=600
+    )
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
